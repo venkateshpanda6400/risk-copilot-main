b9290a2910063b323d11c6da320d939dba1fc07f
Venkatesh Panda <venkatesh.panda@randstaddigital.com>
2024-09-10 03:31:30 +0530

    changing the ports
    changing the ports

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def run_streamlit(script_name, port):
 if __name__ == "__main__":
     # Create two processes
     p1 = multiprocessing.Process(
-        target=run_streamlit, args=("src/riskCopilot/admin.py", 8053)
+        target=run_streamlit, args=("src/riskCopilot/admin.py", 8080)
     )
     p2 = multiprocessing.Process(
         target=run_script, args=("src/riskCopilot/monitor.py",)
     )
 
     p3 = multiprocessing.Process(
-        target=run_streamlit, args=("src/riskCopilot/app.py", 8053)
+        target=run_streamlit, args=("src/riskCopilot/app.py", 8000)
     )
 
     # Start both processes
